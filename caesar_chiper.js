d9853fae8a509c0ebcdda4121ddd10983cd1cd4d
ilmialimudins <ilmi.muhammad@outlook.com>
2023-01-22 06:06:47 +0700

    push caesar_chiper

---
+++ caesar_chiper.js
@@ -0,0 +1,60 @@
+// function bebas(s,k){
+// let n=10
+// console.log(n)
+// let s='muhammadaa'
+// // console.log(s)
+// //   = pxkdppdgdd
+// let k=3
+// console.log()
+
+// i=s.charCodeAt(0)
+// console.log(i)
+
+
+
+// console.log(s.charCodeAt(0))
+// console.log(s.charCodeAt(1))
+// console.log(s.charCodeAt(2))
+// console.log(s.charCodeAt(3))
+// console.log(s.charCodeAt(4))
+// console.log(s.charCodeAt(5))
+// console.log(s.charCodeAt(6))
+// console.log(s.charCodeAt(7))
+// console.log(s.charCodeAt(8))
+// console.log(s.charCodeAt(9))
+let s='middle-Outz',
+k=2
+let hasil='';
+for(let i=0; i<s.length;i++){
+    hasil+= String.fromCharCode(s.charCodeAt(i)+k)
+    newhasil =hasil.replace('/','-');
+    newhasil2 =hasil.replace('|','b');
+}
+console.log(newhasil2);
+       
+//    if(hasil==`/`){
+//     return '-';
+//     }
+   
+
+// charCodeAt= m(string) -> 109(kode acsii)
+// fromCharCode 109(kode ascii) -> m(string)
+
+// awal= [s.charCodeAt(0)]+' '+[s.charCodeAt(1)]+' '+[s.charCodeAt(2)]+' '+[s.charCodeAt(3)]+' '+[s.charCodeAt(4)]+' '+[s.charCodeAt(5)]
+// +[s.charCodeAt(6)]+' '+[s.charCodeAt(7)]+' '+[s.charCodeAt(8)]+' '+[s.charCodeAt(9)]
+// console.log(awal)
+
+// proses= [s.charCodeAt(0)+k]+' '+[s.charCodeAt(1)+k]+' '+[s.charCodeAt(2)+k]+' '+[s.charCodeAt(3)+k]+' '+[s.charCodeAt(4)+k]+' '+[s.charCodeAt(5)]
+// +[s.charCodeAt(6)+k]+' '+[s.charCodeAt(7)+k]+' '+[s.charCodeAt(8)+k]+' '+[s.charCodeAt(9)+k]
+// console.log(proses)
+
+// hasil = String.fromCharCode(112,120, 107, 100, 112, 109,100, 103, 100, 100)
+// console.log(hasil)
+
+
+// for (i=0;i<10;i++){
+// awal = s.charCodeAt(0)
+
+
+// } console.log(awal)
+    
